7129df342d38a3e1c3762d7a373e1d9a9cbe9d91
David J. Hunter <dhunter@westmont.edu>
2022-09-07 12:51:04 -0700

    Vigenere attack slides and more vigtools

--- R/vigtools.R
+++ R/vigtools.R
@@ -59,3 +59,62 @@ vigenere <- function(txt, keyVector)
   )
   return(mod26ToString(ct))
 }
+
+#' Shift a vector by n positions
+#'
+#' This function shifts the elements of a vector n positions to
+#' the right, with the end wrapping around to the beginning.
+#'
+#' @param v Vector to be shifted
+#' @param n Number of positions to shift the vector
+#'
+#' @return The shifted vector
+#' @export
+#'
+#' @examples
+#' shiftVec(c("a","b","c","d","e"), 1)
+#' shiftVec(c(2, 3, 5, 7, 9, 11, 13, 17), 3)
+#'
+shiftVec <- function(v, n) {
+  v[(seq_along(v) - (n+1)) %% length(v) + 1]
+}
+
+#' Find characters in positions n, n+r, n+2r, n+3r, etc
+#'
+#' Given a string, this function returns a new string comprising the 
+#' characters in positions n, n+r, n+2r, n+3r, etc.
+#'
+#' @param txt A string
+#' @param n Position of the first character
+#' @param r Relative position of each subsequent character
+#'
+#' @return A string containing the selected characters
+#' @export
+#'
+#' @examples
+#' skipString("whatshouldwedotodaybrain",3,4)
+#'
+skipString <- function(txt, n, r) {
+  l <- unlist(strsplit(txt,""))
+  ss <- l[seq(n,length(l),r)]
+  return(paste0(ss,collapse=""))
+}
+
+#' Compute the relative frequency of letters in a string
+#'
+#' This function computes the relative frequency each letter occurs 
+#' in a string of lower case letters.
+#'
+#' @param txt String (assumed to contain only lowercase letters)
+#'
+#' @return A vector of letter frequency, in order of lower case English letters.
+#' @export
+#'
+#' @examples
+#' letterFreq("albuquerqueisacityinnewmexico")
+#'
+letterFreq <- function(txt) {
+  l <- unlist(c(strsplit(txt,""),letters))
+  t <- as.vector(table(l))-1
+  return(t/sum(t))
+}
